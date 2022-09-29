30e6a65c01cf260ddfb130f42cdbe5e3b2cc43ce
Saqlain Qureshi <iamkhatik786@gmail.com>
2022-09-29 12:39:45 +0530

    Take a range of 0 - 1000 Numbers and find the Prime numbers in that range.

---
+++ PrimeNumGenerator.java
@@ -0,0 +1,24 @@
+package com.bl.dsa;
+
+public class PrimeNumGenerator {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Prime Numbers Program!");
+		System.out.println("\nGenerating Prime numbers between 0 and 1000 :\n");
+		PrimeNumGenerator call = new PrimeNumGenerator();
+		call.generatePrime(0, 1000);
+	}
+
+	public void generatePrime(int low, int high) {
+		for (int counter = 2; counter <= high; counter++) {
+			int flag = 0;
+			for (int factor = 2; factor < counter; factor++) {
+				if(counter % factor == 0)
+					flag++;
+			}
+			if (flag == 0)
+				System.out.println(counter);
+		}
+	}
+
+}
